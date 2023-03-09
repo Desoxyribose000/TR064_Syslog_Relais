27db5c1366dd712a198960b4269510b0c1a32f31
Desoxyribose000 <nowak.mj@gmx.de>
2023-03-09 20:33:49 +0100

    Test Syslog on Linux

--- tr64CheckLog.py
+++ tr64CheckLog.py
@@ -2,10 +2,32 @@ import requests
 from requests.auth import HTTPDigestAuth
 import syslog
 
+
 HOST = "192.168.0.115"
 PORT = "49000"
-UID = '#########'
+UID = '########'
 PASSWD = '########'
+
+
+
+# ---------------- Implementation for Linux Service --------------------------------------------------------------------
+
+# [Unit]
+# Description = Logging service for Tr-064 Device
+# After = network.target  # Assuming you want to start after network interfaces are made available
+#
+# [Service]
+# Type = simple
+# ExecStart = python < Path to File >
+# User =  # User to run the script as
+# Group =  # Group to run the script as
+# Restart = on - failure  # Restart when there are errors
+# SyslogIdentifier = TR-064 Relais Service
+# RestartSec = 5
+# TimeoutStartSec = infinity
+#
+# [Install]
+# WantedBy = multi - user.target  # Make it accessible to other users
 
 
 # File is intended to be executed as a service or cron job on linux repeatedly
@@ -15,7 +37,17 @@ PASSWD = '########'
 
 # vorraussetzung installiertes syslog monitoring für client
 
+# persistent data in /var/app_name/logs
+
 # secret handling???? file with permissions?
+
+
+# Used for more granular Error handling acording to HTTP Response Code
+class CustomError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return "Error: %s" % self.value
 
 
 def get_log_from_tr64(host, port, uid, passwd):
@@ -34,13 +66,19 @@ def get_log_from_tr64(host, port, uid, passwd):
     locator = "/upnp/control/deviceinfo"
 
     url = "http://" + HOST + ":" + PORT + locator
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload, auth=HTTPDigestAuth(UID, PASSWD))
+        syslog.syslog(syslog.LOG_INFO, response)
+    except Exception:
+        syslog.syslog(syslog.LOG_ERR, Exception)
 
-    response = requests.request("POST", url, headers=headers, data=payload, auth=HTTPDigestAuth(UID, PASSWD))
-
-    return response
-
-
-print(get_log_from_tr64(HOST, PORT, UID, PASSWD).text)
+    try:
+        if response == "<Response [200]>":
+            pass
+        elif response == "<Response [401]>":
+            raise CustomError("Authorization failure.")
+    except CustomError:
+        syslog.syslog(syslog.LOG_ERR, CustomError)
 
 
 def get_logs_from_file(file):
@@ -57,3 +95,6 @@ def compare_logs(file_logs, tr64logs):
 def log_to_syslog_and_file(new_logs):
     # log all new log entries to syslog and add them afterwards to the file of logged messsages
     pass
+
+
+print(get_log_from_tr64(HOST, PORT, UID, PASSWD))
